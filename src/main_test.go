5bb527139e2f221e3e436e64ffe3a1dc7688909c
Paul Leydier <paul.leydier@compass-ft.com>
2022-03-09 22:50:09 +0100

    🔥 remove unit test file cleaning function

--- src/main_test.go
+++ src/main_test.go
@@ -9,19 +9,8 @@ import (
 
 const testTableName = "testTable"
 
-func cleanTestTables() {
-	files, _ := ioutil.ReadDir(localSavePath)
-	for _, file := range files {
-		name := file.Name()
-		if len(name) > len(testTableName) && name[:len(testTableName)] == testTableName {
-			os.Remove(localSavePath + "/" + file.Name())
-		}
-	}
-}
-
 func TestProcessInputValidInsert(t *testing.T) {
 	// Insert a row: table should now contain 1 page and 1 row
-	cleanTestTables()
 	table, _ := openTable(testTableName)
 	err := processInput("insert 1 user1 person1@example.com", table)
 	if err != nil {
@@ -34,7 +23,6 @@ func TestProcessInputValidInsert(t *testing.T) {
 
 func TestProcessInputInsertSelect(t *testing.T) {
 	// Insert a row: select should now print a single line
-	cleanTestTables()
 	table, _ := openTable(testTableName)
 
 	// Mock console output
@@ -66,7 +54,6 @@ func TestProcessInputInsertSelect(t *testing.T) {
 func TestProcessInputTooManyInserts(t *testing.T) {
 	// Insert too many rows: higher than max allowed
 	os.Stdout = nil // Mute output
-	cleanTestTables()
 	table, _ := openTable(testTableName)
 	for i := 0; i < 10000; i++ {
 		err := processInput(fmt.Sprintf("insert %d user%d person%d@example.com", i, i, i), table)
